7aaf4690490a323bd1bf499202a40497aa8fb25b
samtir65 <samtir65@gmail.com>
2023-05-20 00:25:26 +0330

    Add Dtos and Interfaces

---
+++ NatsServer/DTOs/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatsServer.DTOs
+{
+    public class RequestDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ NatsServer/Interfaces/IUserManagement.cs
@@ -0,0 +1,15 @@
+﻿using NatsServer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatsServer.Interfaces
+{
+    public interface IUserManagement
+    {
+        UserDto Signup(RequestDto requestDto);
+        UserDto Get(Guid Id);
+    }
+}
